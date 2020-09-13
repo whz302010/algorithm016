17f3baec8149e77edbafb87e97a40eaf786fa2fb
whz302010 <898430656@qq.com>
2020-09-14 00:36:54 +0800

    合并两个有序链表

---
+++ Week_01/MergeTwoList.java
@@ -0,0 +1,46 @@
+package AlgorithmExercise.easy;
+
+
+/**
+ * @description
+ *
+ * 将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。 
+ *
+ *  
+ *
+ * 示例：
+ *
+ * 输入：1->2->4, 1->3->4
+ * 输出：1->1->2->3->4->4
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/merge-two-sorted-lists
+ * @author:wuhaizhong
+ * @date:2020/9/13
+ */
+public class MergeTwoList {
+
+
+      public static class ListNode {
+          int val;
+          ListNode next;
+          ListNode() {}
+          ListNode(int val) { this.val = val; }
+          ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+      }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1==null){
+            return l2;
+        }else if (l2==null){
+            return l1;
+        }else if (l1.val<l2.val){
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }else {
+            l2.next = mergeTwoLists(l1,l2.next);
+            return l2;
+        }
+    }
+
+}
