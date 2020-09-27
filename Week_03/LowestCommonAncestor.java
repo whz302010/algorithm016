dea6719db45178376c748d7c3c938b5709721516
whz302010 <898430656@qq.com>
2020-09-28 00:12:35 +0800

    第三周二叉树公共祖先提交

---
+++ Week_03/LowestCommonAncestor.java
@@ -0,0 +1,73 @@
+package AlgorithmExercise.Week2.middle;
+
+import AlgorithmExercise.Week2.easy.MinDepth;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * @description
+ * 给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+ *
+ * 百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+ *
+ * 例如，给定如下二叉树:  root = [3,5,1,6,2,0,8,null,null,7,4]
+ *
+ * 示例 1:
+ *
+ * 输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 1
+ * 输出: 3
+ * 解释: 节点 5 和节点 1 的最近公共祖先是节点 3。
+ * 示例 2:
+ *
+ * 输入: root = [3,5,1,6,2,0,8,null,null,7,4], p = 5, q = 4
+ * 输出: 5
+ * 解释: 节点 5 和节点 4 的最近公共祖先是节点 5。因为根据定义最近公共祖先节点可以为节点本身。
+ *  
+ *
+ * 说明:
+ *
+ * 所有节点的值都是唯一的。
+ * p、q 为不同节点且均存在于给定的二叉树中。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree
+ * @author:wuhaizhong
+ * @date:2020/9/26
+ */
+public class LowestCommonAncestor {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    TreeNode node ;
+    /**
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root==p||root==q) {
+            return root;
+        }
+        if (root!=null){
+            TreeNode lNode=lowestCommonAncestor(root.left,p,q);
+            TreeNode rNode=lowestCommonAncestor(root.right,p,q);
+            if (lNode!=null&&rNode!=null)
+                return root;
+            else if(lNode==null) {//两个都在右子树
+                return lowestCommonAncestor(rNode, p, q);
+            }
+            else { //两个都在左子树里面
+                return lowestCommonAncestor(lNode, p, q);
+            }
+        }
+        return null;
+    }
+
+}
